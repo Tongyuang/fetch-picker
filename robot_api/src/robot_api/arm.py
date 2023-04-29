277c63b3cf03762bc593fd77ec19d7321d40cb0f
Yuang Tong <yuangtong1999@gmail.com>
2023-04-28 17:25:56 -0700

    add pose check function

--- robot_api/src/robot_api/arm.py
+++ robot_api/src/robot_api/arm.py
@@ -82,25 +82,79 @@ class Arm(object):
         
         return self.client.get_result()
 
-    def move_to_pose(self,pose_stamped):
-        """Moves the robot's arm to the given pose.
+    def move_to_pose(self,
+                    pose_stamped,
+                    allowed_planning_time=10.0,
+                    execution_timeout=15.0,
+                    group_name='arm',
+                    num_planning_attempts=1,
+                    plan_only=False,
+                    replan=False,
+                    replan_attempts=5,
+                    tolerance=0.01):
+        """Moves the end-effector to a pose, using motion planning.
 
         Args:
             pose: geometry_msgs/PoseStamped. The goal pose for the gripper.
+            allowed_planning_time: float. The maximum duration to wait for a
+                planning result, in seconds.
+            execution_timeout: float. The maximum duration to wait for
+                an arm motion to execute (or for planning to fail completely),
+                in seconds.
+            group_name: string. Either 'arm' or 'arm_with_torso'.
+            num_planning_attempts: int. The number of times to compute the same
+                plan. The shortest path is ultimately used. For random
+                planners, this can help get shorter, less weird paths.
+            plan_only: bool. If True, then this method does not execute the
+                plan on the robot. Useful for determining whether this is
+                likely to succeed.
+            replan: bool. If True, then if an execution fails (while the arm is
+                moving), then come up with a new plan and execute it.
+            replan_attempts: int. How many times to replan if the execution
+                fails.
+            tolerance: float. The goal tolerance, in meters.
 
         Returns:
             string describing the error if an error occurred, else None.
         """
         goal_builder = MoveItGoalBuilder()
+        goal_builder.group_name = group_name
         goal_builder.set_pose_goal(pose_stamped)
+        goal_builder.allowed_planning_time = allowed_planning_time
+        goal_builder.num_planning_attempts = num_planning_attempts
+        goal_builder.plan_only = plan_only
+        goal_builder.replan = replan
+        goal_builder.replan_attempts = replan_attempts
+        goal_builder.tolerance = tolerance
         goal = goal_builder.build() # a MoveGroupGoal
+        
         # try to move to the goal
         self._move_group_client.send_goal(goal)
-        self._move_group_client.wait_for_result(rospy.Duration(10))
+        self._move_group_client.wait_for_result(rospy.Duration(execution_timeout))
         val = self._move_group_client.get_result().error_code.val
+        
         if val == MoveItErrorCodes.SUCCESS:
             return None
         return self.moveit_error_string(val)
+    
+    def check_pose(self,pose_stamped,allowed_planning_time=10.0,group_name='arm',tolerance=0.01):
+        """_summary_
+
+        Args:
+            pose_stamped geometry_msgs/PoseStamped. The goal pose for the gripper.
+            allowed_planning_time (float, optional): The maximum duration to wait for a
+                planning result, in seconds. Defaults to 10.0.
+            group_name (str, optional): Either 'arm' or 'arm_with_torso'. Defaults to 'arm'.
+            tolerance (float, optional): float. The goal tolerance, in meters. Defaults to 0.01.
+        """
+        assert group_name in ['arm','arm_with_torso']
+        return self.move_to_pose(
+            pose_stamped,
+            allowed_planning_time = allowed_planning_time,
+            group_name = group_name,
+            tolerance = tolerance,
+            plan_only = True
+        )
     
     def cancel_all_goals(self):
         self._client.cancel_all_goals() 
